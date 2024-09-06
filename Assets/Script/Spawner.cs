e04853ece6a27d93dc50f149c7044de5ebfb6250
dvbb <578760600@qq.com>
2024-09-06 14:18:15 +0800

    spawner

---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpawnModes
+{
+    Fixed,
+    Random
+}
+
+public class Spawner : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private SpawnModes spawnMode = SpawnModes.Fixed;
+    [SerializeField] private int enemyCount = 10;
+    [SerializeField] private GameObject enemy;
+
+    [Header("Fixed delay")]
+    [SerializeField] private float delayBtwSpawns;
+
+    [Header("Random delay")]
+    [SerializeField] private float minRandomDelay;
+    [SerializeField] private float maxRandomDelay;
+
+    private float _spawnTimer;
+    private int _enemiesSpawned;
+
+    private void Update()
+    {
+        _spawnTimer -= Time.deltaTime;
+        if (_spawnTimer < 0)
+        {
+            _spawnTimer = GetSpawnDelay();
+            if (_enemiesSpawned < enemyCount)
+            {
+                SpawnEnemy();
+                _enemiesSpawned++;
+            }
+        }
+    }
+
+    private float GetSpawnDelay()
+    {
+        float time = 0f;
+        switch (spawnMode)
+        {
+            case SpawnModes.Fixed:
+                time = delayBtwSpawns;
+                break;
+            case SpawnModes.Random:
+                time = GetRandomDelay();
+                break;
+            default:
+                break;
+        }
+        return time;
+    }
+    private void SpawnEnemy() => Instantiate(enemy, transform.position, Quaternion.identity);
+    private float GetRandomDelay() => Random.Range(minRandomDelay, maxRandomDelay);
+}
