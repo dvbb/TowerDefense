6310a44bbd487e2ce0a5ce6fe28792d343e3e7a1
dvbb <578760600@qq.com>
2024-09-09 15:44:14 +0800

    bullet

--- Assets/Script/Bullet/Bullet.cs
+++ Assets/Script/Bullet/Bullet.cs
@@ -1,25 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.EventSystems.EventTrigger;
+using static UnityEngine.GraphicsBuffer;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private float demage = 10;
 
     public Enemy Target { get; private set; }
 
+    public bool canMove;
+
     private void Update()
     {
-        
+        if (canMove)
+            Move();
     }
 
-    private void OnAnimatorMove()
+    public void Move()
     {
-        
+        if (Target == null)
+            return;
+        transform.position = Vector3.MoveTowards(transform.position, Target.transform.position, moveSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, Target.transform.position) < .1)
+        {
+            Target.TakeDemage(demage);
+            ObjectPooler.ReturnToPool(gameObject);
+            RestBullet();
+        }
     }
 
-    public void SetEnemy(Enemy enemy)
+    public void InitBullet(Enemy enemy)
     {
         Target = enemy;
+    }
+
+    public void RestBullet()
+    {
+        transform.localEulerAngles = Vector3.zero;
+        canMove = false;
+        Debug.Log("reset  " + transform.rotation);
     }
 }
--- Assets/Script/Turret/Turret.cs
+++ Assets/Script/Turret/Turret.cs
@@ -1,31 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Turret : MonoBehaviour
 {
     [SerializeField] private float attackRange = 3f;
+    [SerializeField] private Transform spawnPosition;
 
-    [SerializeField] public List<Enemy> EnemyTargets { get; private set; }
+
+    #region Component
+    private ObjectPooler pooler;
+    #endregion
+
+    [Header("Attack info")]
+    [SerializeField] private float coldDown;
+    private float timer;
 
     private bool _isGameStarted;
+    [SerializeField] public List<Enemy> EnemyTargets { get; private set; }
 
     private void Start()
     {
+
+        pooler = GetComponent<ObjectPooler>();
+
         _isGameStarted = true;
         EnemyTargets = new List<Enemy>();
     }
 
     private void Update()
     {
-
+        timer -= Time.deltaTime;
+        if (timer < 0 && EnemyTargets.Count != 0)
+        {
+            timer = coldDown;
+            LoadBullet();
+        }
     }
 
-    private void RotateTowardsTarget(Enemy enemy)
+    private float GetRotateAngle(Enemy enemy)
     {
         Vector3 targetPosition = enemy.transform.position - transform.position;
         float angle = Vector3.SignedAngle(transform.up, targetPosition, transform.forward);
-        transform.Rotate(0, 0, angle);
+        Debug.Log("targetPosition:  " + targetPosition);
+        Debug.Log("angle:  " + angle);
+        //if (angle < 0)
+        //    return -angle;
+        return angle; 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -51,5 +74,20 @@ public class Turret : MonoBehaviour
     {
         if (_isGameStarted)
             GetComponent<CircleCollider2D>().radius = attackRange;
+    }
+
+    private void LoadBullet()
+    {
+        GameObject newInstance = pooler.GetInstanceFromPool();
+        newInstance.transform.localPosition = spawnPosition.position;
+
+        Bullet bullet = newInstance.GetComponent<Bullet>();
+        bullet.InitBullet(EnemyTargets.First());
+        bullet.canMove = true;
+        bullet.transform.Rotate(0, 0, 0);
+        bullet.transform.Rotate(0, 0, GetRotateAngle(EnemyTargets.First()));
+        Debug.Log(bullet.transform.rotation);
+
+        newInstance.SetActive(true);
     }
 }
