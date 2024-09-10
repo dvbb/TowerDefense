bbd806a2ad856d58d321006fe67d6f0983b6fb29
dvbb <578760600@qq.com>
2024-09-10 15:29:30 +0800

    Turret shop UI

---
+++ Assets/Script/TurretShop/TurretSettings.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Turret Shop Setting")]
+public class TurretSettings : ScriptableObject
+{
+    [SerializeField] private GameObject TurretPrefab;
+    [SerializeField] private int TurretShopCost;
+    [SerializeField] private Sprite TurretShopSprite;
+}
